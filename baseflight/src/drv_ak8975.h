28df4c67c5b67ddb160454f56de5a418eb6e3a65
jaejung <mwnjaejung@gmail.com>
2015-02-02 22:38:09 +0900

    Revert "update baseflight source"
    This reverts commit 1e71e3f294e5c3691f47344f257b8adfce8dbe71.

--- baseflight/src/drv_ak8975.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-bool ak8975detect(sensor_t *mag);
-void ak8975Init(sensor_align_e align);
-void ak8975Read(int16_t *magData);
--- baseflight/src/drv_hmc5883l.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-bool hmc5883lDetect(sensor_t *mag);
-void hmc5883lInit(sensor_align_e align);
-void hmc5883lRead(int16_t *magData);
