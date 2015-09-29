72e353a69f578dce66562e0b6cd76072190c694c
jaejung <mwnjaejung@gmail.com>
2015-09-29 13:20:00 +0900

    update Printf_Naze32 project

---
+++ STM32/stm32vldiscovery_package/Project/Examples/Printf_Naze32/main.c
@@ -0,0 +1,46 @@
+
+#include "global.h"
+
+
+#ifdef USE_LAME_PRINTF
+// gcc/GNU version
+static void _putc(void *p, char c)
+{
+    (void)p;
+    serialWrite(g_mainport, c);
+}
+#else
+// keil/armcc version
+int fputc(int c, FILE *f)
+{
+    // let DMA catch up a bit when using set or dump, we're too fast.
+   while (!isSerialTransmitBufferEmpty(g_mainport));
+   serialWrite(g_mainport, c);
+   return c;
+}
+#endif
+
+
+
+int main()
+{
+	// 클럭, 타이머 초기화
+	systemInit();
+	
+	// 시리얼 통신 초기화.
+	serialInit(9600);
+	
+	// 루프
+	while (1)
+	{
+		// 시리얼 통신 처리.
+		serialCom();
+
+		printf( "test\n" );
+		//printf( "1111" );
+		
+		delay(1000);
+	}
+		
+}
+
